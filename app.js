7c15687aa99283958d5d7cea666deee52e0206cf
hansent <thomas.hansen@gmail.com>
2014-02-19 15:58:35 -0600

    MMOP

--- app.js
+++ app.js
@@ -24,8 +24,103 @@ if ('development' == app.get('env')) {
 }
 
 
-io.sockets.on('connection', function(socket){
 
+var p1_y = 300;
+var p2_y = 300;
+var width = 1300;
+var height = 700;
+var ball = {x: width/2, y:height/2, dx:0, dy:0};
+
+max_y = 175;
+min_y = -175;
+
+
+var host_socket = false;
+
+io.sockets.on('connection', function(socket){
+    
+    if (!host_socket){
+        
+        host_socket = socket;
+        host_socket.emit("host", {});
+        host_socket.on('disconnect', function(){
+            host_socket = false;
+        });
+    }
+    
+    
+    
+    //whenever anyone connects, set paddle to middle
+    socket.emit('paddle1', {y: p1_y});
+    socket.emit('paddle2', {y: p2_y});
+    socket.emit('ball', ball);
+    
+    socket.on('ball_data', function(data){
+        if (socket == host_socket){
+            ball = data;
+            socket.broadcast.emit('ball', data);
+        }
+    });
+    
+    
+
+    
+    
+    socket.on('paddle1_move_up', function(data){
+        p1_y = p1_y - 10;
+        if (p1_y >= height - 175)
+          p1_y = height - 175;
+        else if (p1_y <= 175)
+          p1_y = 175;
+        io.sockets.emit('paddle1', {y:p1_y});
+        
+    });
+    
+    socket.on('paddle1_move_down', function(data){
+        p1_y = p1_y + 10;
+        if (p1_y >= height - 175)
+          p1_y = height - 175;
+        else if (p1_y <= 175)
+          p1_y = 175;
+        io.sockets.emit('paddle1', {y:p1_y});
+        
+    });
+
+    
+    socket.on('paddle2_move_up', function(data){
+        p2_y = p2_y - 10;
+        if (p2_y >= height - 175)
+          p2_y = height - 175;
+        else if (p2_y <= 175)
+          p2_y = 175;
+        io.sockets.emit('paddle2', {y:p2_y});
+        
+    });
+    
+    socket.on('paddle2_move_down', function(data){
+        p2_y = p2_y + 10;
+        if (p2_y >= height - 175)
+          p2_y = height - 175;
+        else if (p2_y <= 175)
+          p2_y = 175;
+        io.sockets.emit('paddle2', {y:p2_y});
+        
+    });
+    
+    socket.on('start', function(data){
+        if (ball.dx != 0) return;
+        if (Math.random() < 0.5)
+            ball.dx = width / 3;
+        else
+            ball.dx = -width / 3;
+      
+        io.sockets.emit('ball', ball);
+    }); 
+    
+    
+    
+
+    
 });
 
 
