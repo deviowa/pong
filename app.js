c1d705c6294490f0ee15590ed000c938d1cb0d90
hansent <thomas.hansen@gmail.com>
2014-02-19 13:32:07 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var socket_io = require('socket.io');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+var server = http.createServer(app);
+var io = socket_io.listen(server);
+io.set('log level', 1);
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+
+io.sockets.on('connection', function(socket){
+
+});
+
+
+//start teh server
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+
+
+
+
+
